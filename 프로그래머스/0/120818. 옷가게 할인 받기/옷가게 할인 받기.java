0db0a01b97b88ea1999954ab0e1131169ae5995e
최장우 <97choijw@gmail.com>
2026-02-11 13:35:03 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int price) {
+        if(price < 300000 && price >= 100000) price *= 0.95;
+        else if(price >= 300000 && price < 500000) price *= 0.9;
+        else if(price >= 500000) price *= 0.8;
+        
+        return price;
+    }
+}
