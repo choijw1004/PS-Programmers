f2a4e2e0fecb7871241dbdc22ece77a0c4d82436
최장우 <97choijw@gmail.com>
2026-02-11 22:35:20 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int k) {
+        char[] arr = Integer.toString(num).toCharArray();
+        
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] - '0' == k) return i + 1;
+        }
+        
+        return -1;
+    }
+}
