991626f118167c26b4a1a3950c60a06991b9826c
최장우 <97choijw@gmail.com>
2026-02-11 12:54:27 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int n = num_list.length;
+        
+        int[] ans = new int[n+1];
+        
+        for(int i = 0 ; i < n; i++){
+            ans[i] = num_list[i];
+        }
+        
+        if(num_list[n-1] > num_list[n-2]) ans[n] = num_list[n-1] - num_list[n-2];
+        else ans[n] = num_list[n-1] * 2;
+        
+        return ans;
+    }
+}
