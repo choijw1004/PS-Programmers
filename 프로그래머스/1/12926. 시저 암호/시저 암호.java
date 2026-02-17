3731309dfa9dcca3d283e9829fe7b7f2ae7dd00d
최장우 <97choijw@gmail.com>
2026-02-17 15:28:05 +0900

    [level 1] Title: 시저 암호, Time: 0.90 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(var c : s.toCharArray()){
+            if(c == ' ') sb.append(" ");
+            
+            else if(c >= 'a' && c <= 'z'){
+                sb.append((char)('a' + (c - 'a' + n) % 26));
+            }
+            else{
+                sb.append((char)('A' + (c - 'A' + n) % 26));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
