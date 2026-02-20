898f293ec5dac1717cd9a11ee68aed53b9bdfd11
최장우 <97choijw@gmail.com>
2026-02-20 11:03:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] d = new long[n+1];
+        if(n == 1) return 1;
+        
+        d[0] = 0;
+        d[1] = 1;
+        d[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            d[i] = (d[i-1] + d[i-2]) % 1234567;
+        }
+        return d[n];
+    }
+}
